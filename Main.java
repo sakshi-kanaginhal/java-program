a92ec38b142ff47527c924231ab31b1b8e7e6af2
Your Name <youremail@example.com>
2026-06-04 12:46:30 +0530

    ini

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select Traffic Light");
+        System.out.println("1. Red");
+        System.out.println("2. Yellow");
+        System.out.println("3. Green");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("STOP");
+                break;
+            case 2:
+                System.out.println("READY");
+                break;
+            case 3:
+                System.out.println("GO");
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+        sc.close();
+    }
+}
